d11f1bda8fd499efc1c495e46bac6b9712c68ef3
antmx <antmx1@ntlworld.com>
2015-07-23 00:10:53 +0100

    Added CustomExceptionTests

---
+++ Common.Tests/CustomExceptionTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Netricity.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netricity.Common.Tests
+{
+	[TestFixture()]
+	public class CustomExceptionTests
+	{
+		[Test()]
+		public void CustomExceptionTest()
+		{
+			var ex1 = new CustomException("foo");
+			var ex2 = new CustomException("bar {0} {1}", 1, 2);
+
+			Assert.AreEqual("foo", ex1.Message);
+			Assert.AreEqual("bar 1 2", ex2.Message);
+		}
+	}
+}
